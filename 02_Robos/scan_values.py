1a154618d2fce89d0d1c1a2f01e98c22bc8bc158
doeschbj <bjoern-doeschl@gmx.de>
2020-03-03 13:37:38 +0100

    asd

--- 02_Robos/scan_values.py
+++ 02_Robos/scan_values.py
@@ -2,7 +2,14 @@ import rospy
 from sensor_msgs.msg import LaserScan
 
 def callback(msg):
-	print len(msg.ranges)
+	print ("Values at 0 Grad")
+	print msg.ranges[0]
+	print ("Values at 90 Grad")
+	print msg.ranges[89]
+	print ("Values at 180 Grad")
+	print msg.ranges[179]
+	print ("Values at 360 Grad")
+	print msg.ranges[359]
 
 rospy.init_node('scan_values')
 sub = rospy.Subscriber('/robot1/scan',LaserScan,callback)
