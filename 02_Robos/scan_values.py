708365c5038c8d3f1f945f240efa4b4802f97778
doeschbj <bjoern-doeschl@gmx.de>
2020-06-12 15:19:20 +0200

    fertig

--- 02_Robos/scan_values.py
+++ 02_Robos/scan_values.py
@@ -1,31 +1,63 @@
 import rospy
 import turtle
 import math
+import threading
+import numpy as np
 from sensor_msgs.msg import LaserScan
-t = turtle.Pen()
+
+lock = threading.Lock()
+pos = np.empty(360 , dtype=float)
+maxi = 10
+mini = 0
 
 def callback(msg):
-	global t
-	t.clear()
-	for i in range(360):
-		x = (math.sin(i) * msg.ranges[i]) * 120.0
-		y = (math.cos(i) * msg.ranges[i]) * 120.0
-		t.goto(x,y)
-		t.down()
-		t.circle(2)
-		t.up()
-		t.goto(0.0)
+	global pos
+	global lock
+	global mini
+	global maxi
+	while not lock.acquire():
+		pass
+	try:
+		pos = np.empty(360 , dtype=float)
+		pos = msg.ranges
+		maxi = msg.range_max
+		mini = msg.range_min
+	finally:
+		lock.release()
 
 def main():
-	global t
+	global pos
+	global lock
+	global mini
+	global maxi
+	t = turtle.Pen()
 	t.ht()
 	t.speed(0)
 	t.tracer(8, 0)
-	t.cricle(10)
+	t.circle(10)
 	t.up()
 	rospy.init_node('scan_values')
 	sub = rospy.Subscriber('/robot3/scan',LaserScan,callback)
-	rospy.spin()
+	while not rospy.is_shutdown():
+		while not lock.acquire():
+			pass
+		try:		
+			for i in range(360):
+				if pos[i] > mini and pos[i] < maxi:
+						x = int(-(math.sin(i * math.pi/180) * pos[i]) * 120)
+						y = int((math.cos(i*math.pi/180) * pos[i]) * 120)
+						t.goto(x, y)
+						t.down()
+						t.circle(2)
+						t.up()	
+		finally:
+			lock.release()
+			t.clear()
+			t.goto(0,0)
+			t.down()
+			t.circle(10)
+			t.up()	
+		
 
 if __name__ == '__main__':
 	main()
