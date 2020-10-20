488f6a72ee6082aa53ccdb4f13395e9c6a8d282b
doeschbj <bjoern-doeschl@gmx.de>
2020-10-20 12:17:57 +0200

    pixy

--- ppp/scripts/barcode_scan_pixy.py
+++ ppp/scripts/barcode_scan_pixy.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python
 import sys, os, time
-sys.path.insert(0, "~/catkin_ws/src/uni_db/girlsday/scripts/pixy_api/")
+sys.path.insert(0, "~/catkin_ws/src/uni_db/ppp/scripts/pixy_api/")
 import rospy
 import math
 from ctypes import *
@@ -8,13 +8,13 @@ from pixy_api import pixy
 from pixy_api.pixy import *
 
 pixy.init()
-pixy.change_porg("line")
+pixy.change_prog("line")
 
 barcodes = BarcodeArray(100)
 frame = 0
 
 while 1:
-    line_get_main_features(4,false)
+    line_get_main_features()
     b_count = line_get_barcodes(100, barcodes)
     if b_count > 0:
         for index in range (0, b_count):
