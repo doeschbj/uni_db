13a5b891b2fc3bb54b2226a25531a7bb363f4d9d
doeschbj <bjoern-doeschl@gmx.de>
2020-10-20 10:59:43 +0200

    geruest

--- ppp/include/ppp/main_control.h
+++ ppp/include/ppp/main_control.h
@@ -2,9 +2,23 @@
 #define MAIN_CONTROL_H
 
 #include "ros/ros.h"
+#include <vector>
+#include "std_msgs/Int32MultiArray.h"
+#include <mutex>
+#include "std_msgs/Int32.h"
 
 int robot_nmbr = -1;
+int amofbots = -1;
+std::vector<int> status;
 std::string node_name;
+ros::Publisher status_pub;
+ros::Subscriber status_sub;
+ros::Subscriber scanQR;
+std::mutex status_mutex;
+
 bool getParams(ros::NodeHandle n);
 void extractIntegerWords(std::string str);
+void publishStatus();
+void updateStatus(std_msgs::Int32MultiArray msg);
+void startScan(std_msgs::Int32 msg);
 #endif
--- ppp/src/main_control.cpp
+++ ppp/src/main_control.cpp
@@ -11,16 +11,41 @@ int main(int argc, char *argv[])
     ros::NodeHandle n;
     if(!getParams(n)){return -1;}
     ROS_INFO("Robot number = %d", robot_nmbr);
+    for(int i = 0; i < robot_nmbr; i++){
+        status.push_back(0);
+    }
+    std::stringstream ss;
+    ss << robot_nmbr;
+    status_pub = n.advertise<std_msgs::Int32MultiArray>("/robot/status", 100);
+    status_sub = n.subscribe("/robot/status", 100, updateStatus);
+    scanQR = n.subscribe("/robot"+ss.str()+"/scanQR", 100, startScan);
 
-    
-    ros::spin();
+
+    ros::AsyncSpinner spinner(6); // Use 3 threads
+    spinner.start();
     return 0;
+}
+
+void startScan(std_msgs::Int32 msg){
+    int data = msg.data;
+    if(data == 1){
+        //startScanQr
+        //mit drehen rechts links usw
+        //unterscheide hier roboter
+        //roboter -1
+        //erkenne station warte festgelegten wert evtl neu 
+        //kalibrieen während warten.
+        //publishb einmal 51 für wartn und 50 ende warten
+    }
 }
 
 bool getParams(ros::NodeHandle n){
     node_name = ros::this_node::getName();
     extractIntegerWords(node_name);
-
+    if(!(n.getParam("/HeadController/num_robots", robot_nmbr))){
+        ROS_ERROR("Could not get number of robots");
+        return false;
+    }
     return true;
 }
 
@@ -32,3 +57,16 @@ void extractIntegerWords(std::string str) {
         );
         robot_nmbr = stoi(output);
 } 
+
+void publishStatus(){
+    std_msgs::Int32MultiArray msg;
+    status_mutex.lock();
+    msg.data = status;
+    status_mutex.unlock();
+    status_pub.publish(msg);
+}
+void updateStatus(std_msgs::Int32MultiArray msg){
+    status_mutex.lock();
+    status = msg.data;
+    status_mutex.unlock();
+}
